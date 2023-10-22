1c34092bd0dfbfab708bf18a3d79e3e7160d3d65
Michael Nguyen <cuong.nguyen@savvycomsoftware.com>
2023-10-22 16:22:31 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['*'],
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   });
 
   const config = new DocumentBuilder()
